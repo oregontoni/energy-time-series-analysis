d980e3644a4b1ad87495ceae314b4ad044a0edd8
thetonic <t.chan13@gmail.com>
2024-04-16 00:19:12 +0100

    completed code for doughnut chart

--- missionzero_app.py
+++ missionzero_app.py
@@ -1,14 +1,16 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import plotly.graph_objs as go
 import streamlit as st
 import joblib
 
-st.title("Mission Zero") 
+st.title("MISSION ZERO") 
 
-st.markdown("<h1 style='text-align: center; color: blue;'>UK Net Zero 2050: Will there be enough renewable energy?</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; color: green; font-style: italic; margin-bottom: 30px'>UK Net Zero 2050: Will there be enough renewable energy?</h1>", unsafe_allow_html=True)
 
-#write a subheader
-st.subheader('Time Series Analysis on National Grid ESO Generation Data (2009 - 2024)')      
-
+#write a subheader     
+st.markdown("<h2 style='text-align: center;  font-size: 16px; margin-bottom: 30px'>Time Series Analysis on National Grid ESO Generation Data (2009 - 2024)</h2>",
+            unsafe_allow_html=True)
 
 model = joblib.load('wind_generation_SARIMA_model.pkl')
 
@@ -27,96 +29,55 @@ forecast = model.forecast(steps=steps_test)
 #data is in half-hourly records; divide by 2 to get MWh
 selected_yr_forecasted_mwh = forecast.loc[str(year_to_forecast)].sum() / 2
 
-#############################################################################
-
-#DOUGHNUT CHART
+#calculate TWh
+selected_yr_forecasted_twh = selected_yr_forecasted_mwh/1000000
 
 #load .csv of non-wind generation in 2023
 non_wind_data = pd.read_csv('energy_gen_2023_excl_wind.csv')
 
-#display MWh forecast for wind
-st.write(f'Forecasted Total Wind Generation for {year_to_forecast}: {selected_yr_forecasted_mwh} MWh')
-
 #add wind calculations to dataframe
-non_wind_data.loc[len(non_wind_data)] = ['Wind', selected_yr_forecasted_mwh]
+non_wind_data.loc[len(non_wind_data)] = ['WIND', selected_yr_forecasted_mwh]
 
 #calculate total energy generation
 total_generation = non_wind_data['Generation (MWh)'].sum()
 
 #calculate total energy opex
 total_cost = selected_yr_forecasted_mwh*176
+total_cost_bil = total_cost/1000000000
+
+############################################################################
+#DOUGHNUT CHART
 
 #create doughnut plot
-fig, ax = plt.subplots()
-wedges, text, autotexts = ax.pie(
-    non_wind_data['Generation(MWh)'],
-    labels=non_wind_data['Energy Sources'],
-    autopct='%1.1f%%',
-    startangle=140,
-    wedgeprops=dict(width=0.3)
-)
+colors_dict = {'GAS': 'firebrick', 'NUCLEAR': 'greenyellow', 'IMPORTS':'turquoise', 'BIOMASS': 'darkviolet', 'SOLAR': 'gold', 'HYDRO': 'lightseagreen', 'COAL': 'dimgray',   
+          'STORAGE': 'salmon', 'WIND': 'royalblue'}
 
-#doughnut center
-center_circle = plt.Circle((0,0), 0.70, fc='darkgreen'))
-fig = plt.gcf()
-fig.gca().add_artist(center_circle)
+values = non_wind_data['Generation (MWh)'].tolist()
+labels = non_wind_data['Energy Sources'].tolist()
+colors = [colors_dict.get(label, 'default_color') for label in labels]
 
-#display total cost + MWh
-plt.text(0, 0, f"£{total_cost} opex \n{selected_yr_forecasted_mwh} MWh",
-         ha='center', va='center', fontsize=17, color='white')
-plt.title(f'UK Energy Mix')
-plt.show()
-                    
+fig = go.Figure(data=[go.Pie(labels=labels, values=values, hole=0.5, 
+                             pull=[0, 0, 0, 0, 0, 0, 0, 0, 0], 
+                             marker_colors=colors, 
+                             textinfo='label',
+                             outsidetextfont=dict(color='black'),
+                             hoverinfo='percent',
+                             hoverlabel=dict(font=dict(size=16, color='black')),
+                             showlegend=False)])
+
+fig.update_layout(title={'text': f'UK Energy Mix in {year_to_forecast} <br><span style="font-size: 18px; font-style: italic;"> (assuming static 2023 generation for non-wind energy sources)</span>', 
+                         'y':0.95, 'x':0.5, 'xanchor': 'center', 'yanchor': 'top', 'font_size': 30}, uniformtext_minsize=15, uniformtext_mode='hide', 
+                         annotations=[
+                             dict(text=f'Wind OpEx <br>', x=0.5, y=0.62, font=dict(size=22, color='black'), showarrow=False), 
+                             dict(text=f'<b>£{round(total_cost_bil,0)} bil<b> <br>', x=0.5, y=0.5, font=dict(size=45, color='black'), showarrow=False), 
+                             dict(text=f'{round(selected_yr_forecasted_twh,0)} TWh', x=0.5, y=0.38, font=dict(size=30, color='black'), showarrow=False, 
+                             textangle=0, align='center')], width=850, height=750,
+                             margin=dict(t=125, b=75, l=0, r=0))
+
+fig.update_traces(textfont=dict(size=16))
+
+fig.show()
 
 #display doughnut chart in Streamlit
-st.pyplot(fig)
+st.plotly_chart(fig)
 
-
-"""
-st.map(df)  v
-
-def load_data(path, num_rows):
-
-    df = pd.read_csv(path, nrows=num_rows) #dictate number of rows passed in
-
-    df = df.rename(columns={'Start Station Latitude': 'lat', 'Start Station Longitude': 'lon'})     
-    df['Start Time'] = pd.to_datetime(df['Start Time'])      # reset dtype for column
-     
-    return df
-
-### B. Load first 50K rows
-df = load_data("wind_gen_model_data_feed.csv", 46)
-
-### C. Display the dataframe in the app
-st.dataframe(df)
-
-#############################################################################################################
-st.subheader("Wind generation forecast using a pretrained SARIMA model")
-
-# A. Load the model using lib
-# load model
-model = joblib.load("wind_generation_SARIMA_model.pkl")
-
-# B. Set up input field
-text = st.text_input('Wind generation forecast to 2050: # of years (enter a number between 1 and 26)', '2')
-
-# C. Use the model to predict & write result
-# write code to do something with the text entered
-
-#forecast
-#expect iterable object, so wrap it as a list using[ ]
-forecast = y_train_fw3.forecast(steps=[text])
-
-#display depending on if 1 or 0, positive or negative
-#if text 1:
-   # st.write("Positive Review")
-
-#else:
-  #  st.write("Negative Review")
-
-#WHAT IF prediction?
-prediction = model.predict_proba([text])
-
-#display depending on if 1 or 0, positive or negative
-st.write(f"Probability of positive sentiment: {round(prediction[0][1],2)}")
-"""
