60e450fa1c75011af782144f48fa74a35e648351
thetonic <t.chan13@gmail.com>
2024-04-18 15:46:33 +0100

    removed home page

--- missionzero_home_app.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-wind_turbine = open('wind_turbine.mp4', 'rb')
-video_bytes = wind_turbine.read()
-wind_turbine.close()
-
-st.video(wind_turbine, loop=True)
-st.markdown("<div style='margin-bottom: 30px;'></div>", unsafe_allow_html=True)
